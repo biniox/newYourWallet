5fe04bb35dbfe40230f519e4d599c070511318a8
Jarek <biniox2@gmail.com>
2020-11-01 20:29:31 +0100

    inital layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,12 @@
 import './App.scss';
 
 import Menu from './../../layout/menu/Menu';
+import Header from '../../layout/header/Header';
 
 const App = () =>  (
     <div className="App" >
         <Menu/>
+        <Header/>
     </div>
   );
 
